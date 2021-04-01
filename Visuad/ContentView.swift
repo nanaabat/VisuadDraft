99f6e34923b9a394bd81059456d9a7001fa1558d
nanaprewa <turkson.nanaabaa@gmail.com>
2021-04-01 14:26:39 -0400

    Initial Commit

---
+++ Visuad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Visuad
+//
+//  Created by Nana Aba Turkson on 4/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Visuad/VisuadApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisuadApp.swift
+//  Visuad
+//
+//  Created by Nana Aba Turkson on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct VisuadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
